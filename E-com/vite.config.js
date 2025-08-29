23d2e24dd67aa37c5ae6c942196911830efcdd00
Hussain Manj <manjhussain950@gmail.com>
2025-08-29 21:26:55 +0500

    done

--- E-com/vite.config.js
+++ E-com/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',        // ensure assets resolve correctly
+  build: { outDir: 'dist' } 
 })
